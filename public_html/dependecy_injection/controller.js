f4c19df34221c96fc81c79ed9095bf8abce0c557
jdperea <jdperea59@gmail.com>
2015-07-06 03:38:09 -0500

    Se agregan las partes 6 y 7 del curso

--- public_html/dependecy_injection/controller.js
+++ public_html/dependecy_injection/controller.js
@@ -1,6 +1,8 @@
 var app = angular.module("MyFirstApp",[]);
-app.controller("FirstController", ["$scope",function(m){
+app.controller("FirstController", ["$scope", "$http" ,function(m, h){
+    /*Es una buena practica comprimir el js para pasarlo a producción*/
     m.nombre = "Juandp";
+    var nameu = h.toString(m.nombre);
     m.nuevoComentario = {};
     m.comentarios = [
         {
---
+++ public_html/http/controller.js
@@ -0,0 +1,5 @@
+var app = angular.module("MyFirstApp",[]);
+app.controller("FirstController", ["$scope", "$http" ,function(m, h){
+    
+    
+}]);

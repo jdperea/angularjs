b4e44debcf6243ce0a7221e4a55b9cd98967366b
jdperea <jdperea59@gmail.com>
2015-07-12 05:18:27 -0500

    Se agrega la sesion 17 ng-src

---
+++ public_html/ng-src1/app.js
@@ -0,0 +1,11 @@
+angular.module("CustomDirective",[])
+
+        .controller("AppCtrl", function($scope, $http){
+            $http.get("https://api.github.com/users/jdperea/repos")
+            .success(function(data){
+                $scope.repos = data;
+            })
+                    .error(function(err){
+                        console.log(err);
+            })
+});
